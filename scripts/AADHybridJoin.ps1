bc4d19893b62a94ed544e22e5fcb992cb71a41c0
Colin Huckstep <31158953+colinhuckstep@users.noreply.github.com>
2026-04-03 13:10:05 -0400

    Update AADHybridJoin.ps1

--- scripts/AADHybridJoin.ps1
+++ scripts/AADHybridJoin.ps1
@@ -1,1 +1,13 @@
 dsregcmd /join /debug | Out-File -FilePath "C:\Windows\Temp\dsregcmd-join-osd.log" -Encoding UTF8
+
+$taskName = "\Microsoft\Windows\Workplace Join\Automatic-Device-Join"
+
+try {
+    $task = Get-ScheduledTask -TaskName "Automatic-Device-Join" -TaskPath "\Microsoft\Windows\Workplace Join\" -ErrorAction Stop
+    Start-ScheduledTask -InputObject $task
+    "[$(Get-Date)] Started scheduled task: $taskName" | Out-File -FilePath "C:\Windows\Temp\dsregcmd-join-osd.log" -Append -Encoding UTF8
+}
+catch {
+    "[$(Get-Date)] FAILED to start scheduled task $taskName : $($_.Exception.Message)" | Out-File -FilePath "C:\Windows\Temp\dsregcmd-join-osd.log" -Append -Encoding UTF8
+    exit 1
+}
